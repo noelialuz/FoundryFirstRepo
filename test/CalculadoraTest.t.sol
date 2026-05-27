2f2546d90155402f9947d9299d0155dd23c6e645
“noelialuz” <noelialuzfernandez@gmail.com>
2026-05-27 16:18:55 +0200

    fix

--- test/CalculadoraTest.t.sol
+++ test/CalculadoraTest.t.sol
@@ -29,16 +29,16 @@ contract CalculadoraTest is Test { //"IS" es la palabra reservada para herencia 
         assert(firstResult_ == firstResult);
     }
     function test_add() public { //"test_" es la palabra reservada para que el compilador reconozca que es un test.
-        uint256 firstNumber = 10;
-        uint256 secondNumber = 20;
-        calculadora.add(firstNumber, secondNumber);
-        assert(calculadora.result() == (firstNumber + secondNumber));
+        uint256 firstNumber_ = 10;
+        uint256 secondNumber_ = 20;
+        calculadora.add(firstNumber_, secondNumber_);
+        assert(calculadora.result() == (firstNumber_ + secondNumber_));
     }
     function test_subtract() public { 
-        uint256 firstNumber = 40;
-        uint256 secondNumber = 20;
-        calculadora.subtract(firstNumber, secondNumber);
-        assert(calculadora.result() == (firstNumber - secondNumber));
+        uint256 firstNumber_ = 40;
+        uint256 secondNumber_ = 20;
+        calculadora.subtract(firstNumber_, secondNumber_);
+        assert(calculadora.result() == (firstNumber_ - secondNumber_));
     }
     function test_multiply() public { 
         uint256 firstNumber = 10;
@@ -47,28 +47,28 @@ contract CalculadoraTest is Test { //"IS" es la palabra reservada para herencia 
         assert(calculadora.result() == (firstNumber * secondNumber));
     }
     function test_divide() public { 
-        uint256 firstNumber = 200;
-        uint256 secondNumber = 20; 
+        uint256 firstNumber_ = 200;
+        uint256 secondNumber_ = 20; 
         vm.prank(owner);
-        calculadora.divide(firstNumber, secondNumber);
-        assert(calculadora.result() == (firstNumber / secondNumber));
+        calculadora.divide(firstNumber_, secondNumber_);
+        assert(calculadora.result() == (firstNumber_ / secondNumber_));
     }
 
         // divide sin ser owner
     function test_revert_divide_notOwner() public {
-        uint256 firstNumber = 100;
-        uint256 secondNumber = 10;
+        uint256 firstNumber_ = 100;
+        uint256 secondNumber_ = 10;
         vm.expectRevert("Not allowed");
-        calculadora.divide(firstNumber, secondNumber);
+        calculadora.divide(firstNumber_, secondNumber_);
     }
 
     // divide con divisor 0
     function test_revert_divide_byZero() public {
         vm.prank(owner);  
-        uint256 firstNumber = 100;
-        uint256 secondNumber = 0;
+        uint256 firstNumber_ = 100;
+        uint256 secondNumber_ = 0;
         vm.expectRevert("Number cannot be zero");
-        calculadora.divide(firstNumber, secondNumber);
+        calculadora.divide(firstNumber_, secondNumber_);
     }
 
 }
